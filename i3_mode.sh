a7de3dedd2c85289967f8bb27f9b3ac0d808e92b
wowmao <oriondo.abp@gmail.com>
2023-07-01 23:27:19 +0800

    Add borderless window style

---
+++ i3_mode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+sudo mkdir -p /usr/share/themes/i3mode/xfwm4
+sudo touch /usr/share/themes/i3mode/xfwm4/themerc
